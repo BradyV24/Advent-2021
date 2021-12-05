a3e972c17c172fbea3de21a813f87a880e0f7ca2
BradyV24 <bradyvanderzalm@gmail.com>
2021-12-04 22:50:42 -0800

    Day 5

---
+++ src/test/java/Day5Tests.java
@@ -0,0 +1,10 @@
+import Day5.Day5;
+import org.junit.jupiter.api.Test;
+
+public class Day5Tests {
+
+    @Test
+    void test1() {
+        Day5.day5();
+    }
+}
---
+++ src/test/java/Day6Tests.java
@@ -0,0 +1,11 @@
+import Day6.Day6;
+import org.junit.jupiter.api.Test;
+
+public class Day6Tests {
+
+    @Test
+    void test1() {
+        Day6.day6_1();
+        Day6.day6_2();
+    }
+}
