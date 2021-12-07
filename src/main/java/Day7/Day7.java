d6287dee932abd700813d832e387fdbb2664e066
BradyV24 <bradyvanderzalm@gmail.com>
2021-12-06 21:58:22 -0800

    Day7

--- src/main/java/Day7/Day7.java
+++ src/main/java/Day7/Day7.java
@@ -9,8 +9,7 @@ import java.util.List;
 public class Day7 {
     public static void day7() {
         List<Integer> in = Arrays.stream(Input.parseDocString("resources/2021/day7").get(0).split(",")).map(y -> Integer.parseInt(y)).toList();
-        List<Integer> fuels1 = new ArrayList<>();
-        List<Integer> fuels2 = new ArrayList<>();
+        List<Integer> fuels1 = new ArrayList<>(), fuels2 = new ArrayList<>();
         for (int[] i = {0}; i[0] < in.stream().max(Integer::compareTo).get(); i[0]++) {
             fuels1.add(in.stream().map(x -> Math.abs(i[0] - x)).reduce(0, Integer::sum));
             fuels2.add(in.stream().map(x -> Math.abs(i[0] - x) * (Math.abs(i[0] - x) + 1) / 2).reduce(0, Integer::sum));
